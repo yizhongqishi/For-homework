55b23e26ac8d82fbb38c3600f4e60f6386ee13f7
yizhongqishi <yizhongqishi@163.com>
2016-06-03 13:09:36 +0800

    the new

--- cy/mainwindow.h
+++ cy/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QButtonGroup>
 
 namespace Ui {
 class MainWindow;
@@ -14,7 +15,21 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    QButtonGroup *BG;
+    QButtonGroup *BGL;
+    QButtonGroup *BGC;
 
+private slots:
+    void on_pushButton_clicked();
+    
+    void on_pushButton_2_clicked();
+
+    void theLine(double);
+
+    void theCircle(double);
+
+    void erro();
+    
 private:
     Ui::MainWindow *ui;
 };
